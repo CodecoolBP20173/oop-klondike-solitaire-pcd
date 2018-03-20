15951c338e02ddd77947217a277633643833d57d
Adel Kovacs <kovacsadel12@gmail.com>
2018-03-20 14:33:22 +0100

    restart method without invoke

--- src/com/codecool/klondike/Klondike.java
+++ src/com/codecool/klondike/Klondike.java
@@ -1,9 +1,16 @@
 package com.codecool.klondike;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
 
 public class Klondike extends Application {
 
@@ -11,18 +18,31 @@ public class Klondike extends Application {
     private static final double WINDOW_HEIGHT = 900;
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 
-    @Override
-    public void start(Stage primaryStage) {
+
+    public void startGame(Stage primaryStage) {
         Card.loadCardImages();
         Game game = new Game();
         game.setTableBackground(new Image("/table/green.png"));
+        game.placeButton();
+
 
         primaryStage.setTitle("Klondike Solitaire");
         primaryStage.setScene(new Scene(game, WINDOW_WIDTH, WINDOW_HEIGHT));
         primaryStage.show();
+
     }
+
+    public void restart(Stage stage) {
+        startGame(stage);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        startGame(primaryStage);
+    }
+
 
 }
